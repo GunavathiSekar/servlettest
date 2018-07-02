9538484547dc1f23eb4e9beb655f2310476f10cc
GunavathiSekar <34473868+GunavathiSekar@users.noreply.github.com>
2018-07-02 17:22:46 +0530

    Add files via upload

---
+++ crud/src/main/java/DbUtil.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DbUtil {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/UserDB","root", "");
+            } catch(SQLException e)
+            {
+                System.out.println(e);
+            }
+            return connection;
+        }
+
+    }
+}
